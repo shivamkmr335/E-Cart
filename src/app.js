8b8acfb9e01abc1c120aa1db38a249ff03c1a25d
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2021-04-01 16:24:30 +0530

    change loaction of index.html and converted it to .hbs and using express rendered index.hbs on live server

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express= require("express");
+const app=express();
+const path=require("path");
+const hbs= require("hbs");
+
+
+const port=process.env.PORT || 3000;
+
+const static_path=path.join(__dirname,"../public");
+const template_path=path.join(__dirname,"../templates/views");
+const partials_path=path.join(__dirname,"../templates/partials");
+
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
+app.set("views",template_path);
+hbs.registerPartials(partials_path);
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.listen(port,()=>{
+    console.log(`server is running at port no ${port}`);
+});
